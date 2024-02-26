ffdc704616b97bb703e9f09390c2995e529e4b91
kim-minsol <rlaalsthf02@naver.com>
2024-02-26 13:56:22 +0000

    updated NeRF

--- NeRF/main.py
+++ NeRF/main.py
@@ -1,311 +1,110 @@
-from load_llff import load_llff_data
-from embedding import get_embedder
+import torch
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 from model import NeRF
 
-def batchify(fn, chunk):
-    if chunk is None:
-        return fn
-    def ret(inputs):
-        return torch.cat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
-    return ret
+
+def accumulate_transmittance(alphas):
+    accumulated = torch.cumprod(alphas, 1)
+    return torch.cat((torch.ones((accumulated.shape[0], 1), device=alphas.device), accumulated[:,:-1]), dim=-1)
 
 
-def run_network(inputs, viewdirs, fn, embed_fn, embeddirs_fn, netchunk=1024*64):
-    inputs_flat = torch.reshape(inputs, [-1, inputs.shape[-1]])
+def render_rays(nerf, rays_o, rays_d, device='cuda', near=0, far=1, N_samples=192):
+    z_val = torch.linspace(near, far, N_samples, device=device).expand(rays_o.shape[0], N_samples)
+    # stratified sampling
+    mid = (z_val[:, 1:] + z_val[:, :-1]) * .5
+    lower = torch.cat([z_val[...,:1], mid], dim=-1)
+    upper = torch.cat([mid, z_val[...,-1:]], dim=-1)
+    t_rand = torch.rand(z_val.shape, device=device)
+    t = lower + (upper - lower) * t_rand  # reparameterization trick / (batch_size, N_samples)
 
-    embedded = embed_fn(inputs_flat)
-    if viewdirs is not None:
-        input_dirs = viewdirs[:, None].expand(inputs.shape)
-        input_dirs_flat = torch.reshape(input_dirs, [-1, inputs.shape[-1]])
-        embedded_dirs = embed_fn(input_dirs_flat)
-        embedded = torch.cat([embedded, embedded_dirs], -1)
-    
-    outputs_flat = batchify(fn, netchunk)(embedded)
-    outputs = torch.reshape(outputs_flat, list(input.shape[:-1]) + [outputs_flat.shape[-1]])
+    x = rays_o.unsqueeze(1) + rays_d.unsqueeze(1) * t.unsqueeze(2) # (batch_size, N_samples, 3)
+    # x = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None]
 
-    return outputs
+    rays_d = rays_d.unsqueeze(1).expand(rays_d.shape[0], N_samples, 3) # (batch_size, 3) -> (batch_size, N_samples, 3)
+
+    colors, sigma = nerf(x.reshape(-1, 3), rays_d.reshape(-1, 3))
+    colors = colors.reshape(x.shape)
+    sigma = sigma.reshape(x.shape[:-1])
+
+    delta = torch.cat((z_val[:, 1:] - z_val[:, :-1], torch.tensor([1e10], device=device).expand(rays_d.shape[0], 1)), -1) # (batch_size, N_samples)
+    alpha = 1 - torch.exp(-sigma * delta)
+    weights = accumulate_transmittance(1 - alpha). unsqueeze(2) * alpha.unsqueeze(2)
+    c = torch.sum(weights * colors, dim=1)
+    weight_sum = weights.sum(-1).sum(-1)
+
+    return c + 1 - weight_sum.unsqueeze(-1)
 
 
-def create_nerf(args):
-    embef_fn, input_ch = get_embedder(args.multires, args.i_embed)  # positional encoding
-
-    input_ch_views = 0
-    embeddirs_fn = None
-    # viewdirs
-    if args.use_viewdirs:
-        embeddirs_fn, input_ch_views = get_embedder(args.multires_views, args.i_embed)
-    output_ch = 5 if args.N_importance > 0 else 4
-    skips = [4] ### ????
-    model = NeRF(D=args.netdepth, W=args.netwidth, input_ch=input_ch, output_ch=output_ch, skips=skips,
-                 input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs).to(device) 
-    grad_vars = list(model.parameters())
-
-    model_fine = None
-    if args.N_importance > 0:
-        model_fine = NeRF(D=args.netdepth, W=args.netwidth, input_ch=input_ch, output_ch=output_ch, skips=skips,
-                 input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs).to(device) 
-        grad_vars += list(model_fine.parameters())
-
-    def network_query_fn(inputs, viewdirs, network_fn): return run_network(
-        inputs, viewdirs, network_fn,
-        embed_fn=embed_fn,
-        embeddirs_fn=embeddirs_fn,
-        netchunk=args.netchunk
-    )
-
-    optimizer = torch.optim.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
-
-    start = 0
-    basedir = args.basedir
-    expname = args.expname
-    ############################
-
-    if args.ft_path is not None and args.ft_path != 'None':
-        ckpts = [args.ft_path]
-    else:
-        ckpts = [os.path.join(basedir, expname, f) for f in sorted(os.listdir(os.path.join(basedir, expname))) if 'tar' in f]
-    
-    print('Found ckpts', ckpts)
-    if len(ckpts) > 0 and not args.no_reload:
-        ckpt_path = ckpts[-1]
-        print('Reloading from', ckpt_path)
-        ckpt = torch.load(ckpt_path)
-
-        start = ckpt['global_step']
-        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
-
-        # load model
-        model.load_state_dict(ckpt['network_fn_state_dict'])
-        if model_fine is not None:
-            model_fine.load_state_dict(ckpt['network_fine_state_dict'])
-    #########################
-    render_kwargs_train = {
-        'network_query_fn' : network_query_fn,
-        'perturb' : args.perturb,
-        'N_importance' : args.N_importance,
-        'network_fine' : model_fine,
-        'N_samples' : args.N_samples,
-        'network_fn' : model,
-        'use_viewdirs' : args.use_viewdirs,
-        'white_bkgd' : args.white_bkgd,
-        'raw_noise_std' : args.raw_noise_std,
-    }
-
-    # NDC only good for LLFF-style forward facing data !
-
-    render_kwargs_test = {
-        k : render_kwargs_train[k] for k in render_kwargs_train
-    }
-    render_kwargs_test['perturb'] = False
-    render_kwargs_test['raw_noise_std'] = 0.
-
-    return render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer
-
-def config_parser():
-    import configargparse
-    parser = configargparse.ArgumentParser()
-    parser.add_argument('--config', is_config_file=True, 
-                        help='config file path')
-    parser.add_argument("--expname", type=str, 
-                        help='experiment name')
-    parser.add_argument("--basedir", type=str, default='./logs/', 
-                        help='where to store ckpts and logs')
-    parser.add_argument("--datadir", type=str, default='./data/llff/fern', 
-                        help='input data directory')
-
-    # training options
-    parser.add_argument("--netdepth", type=int, default=8, 
-                        help='layers in network')
-    parser.add_argument("--netwidth", type=int, default=256, 
-                        help='channels per layer')
-    parser.add_argument("--netdepth_fine", type=int, default=8, 
-                        help='layers in fine network')
-    parser.add_argument("--netwidth_fine", type=int, default=256, 
-                        help='channels per layer in fine network')
-    parser.add_argument("--N_rand", type=int, default=32*32*4, 
-                        help='batch size (number of random rays per gradient step)')
-    parser.add_argument("--lrate", type=float, default=5e-4, 
-                        help='learning rate')
-    parser.add_argument("--lrate_decay", type=int, default=250, 
-                        help='exponential learning rate decay (in 1000 steps)')
-    parser.add_argument("--chunk", type=int, default=1024*32, 
-                        help='number of rays processed in parallel, decrease if running out of memory')
-    parser.add_argument("--netchunk", type=int, default=1024*64, 
-                        help='number of pts sent through network in parallel, decrease if running out of memory')
-    parser.add_argument("--no_batching", action='store_true', 
-                        help='only take random rays from 1 image at a time')
-    parser.add_argument("--no_reload", action='store_true', 
-                        help='do not reload weights from saved ckpt')
-    parser.add_argument("--ft_path", type=str, default=None, 
-                        help='specific weights npy file to reload for coarse network')
-
-    # rendering options
-    parser.add_argument("--N_samples", type=int, default=64, 
-                        help='number of coarse samples per ray')
-    parser.add_argument("--N_importance", type=int, default=0,
-                        help='number of additional fine samples per ray')
-    parser.add_argument("--perturb", type=float, default=1.,
-                        help='set to 0. for no jitter, 1. for jitter')
-    parser.add_argument("--use_viewdirs", action='store_true', 
-                        help='use full 5D input instead of 3D')
-    parser.add_argument("--i_embed", type=int, default=0, 
-                        help='set 0 for default positional encoding, -1 for none')
-    parser.add_argument("--multires", type=int, default=10, 
-                        help='log2 of max freq for positional encoding (3D location)')
-    parser.add_argument("--multires_views", type=int, default=4, 
-                        help='log2 of max freq for positional encoding (2D direction)')
-    parser.add_argument("--raw_noise_std", type=float, default=0., 
-                        help='std dev of noise added to regularize sigma_a output, 1e0 recommended')
-
-    parser.add_argument("--render_only", action='store_true', 
-                        help='do not optimize, reload weights and render out render_poses path')
-    parser.add_argument("--render_test", action='store_true', 
-                        help='render the test set instead of render_poses path')
-    parser.add_argument("--render_factor", type=int, default=0, 
-                        help='downsampling factor to speed up rendering, set 4 or 8 for fast preview')
-
-    # training options
-    parser.add_argument("--precrop_iters", type=int, default=0,
-                        help='number of steps to train on central crops')
-    parser.add_argument("--precrop_frac", type=float,
-                        default=.5, help='fraction of img taken for central crops') 
-
-    # dataset options
-    parser.add_argument("--dataset_type", type=str, default='llff', 
-                        help='options: llff / blender / deepvoxels')
-    parser.add_argument("--testskip", type=int, default=8, 
-                        help='will load 1/N images from test/val sets, useful for large datasets like deepvoxels')
-
-    ## deepvoxels flags
-    parser.add_argument("--shape", type=str, default='greek', 
-                        help='options : armchair / cube / greek / vase')
-
-    ## blender flags
-    parser.add_argument("--white_bkgd", action='store_true', 
-                        help='set to render synthetic data on a white bkgd (always use for dvoxels)')
-    parser.add_argument("--half_res", action='store_true', 
-                        help='load blender synthetic data at 400x400 instead of 800x800')
-
-    ## llff flags
-    parser.add_argument("--factor", type=int, default=8, 
-                        help='downsample factor for LLFF images')
-    parser.add_argument("--no_ndc", action='store_true', 
-                        help='do not use normalized device coordinates (set for non-forward facing scenes)')
-    parser.add_argument("--lindisp", action='store_true', 
-                        help='sampling linearly in disparity rather than depth')
-    parser.add_argument("--spherify", action='store_true', 
-                        help='set for spherical 360 scenes')
-    parser.add_argument("--llffhold", type=int, default=8, 
-                        help='will take every 1/N images as LLFF test set, paper uses 8')
-
-    # logging/saving options
-    parser.add_argument("--i_print",   type=int, default=100, 
-                        help='frequency of console printout and metric loggin')
-    parser.add_argument("--i_img",     type=int, default=500, 
-                        help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=10000, 
-                        help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=50000, 
-                        help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000, 
-                        help='frequency of render_poses video saving')
-
-    return parser
-    
-def train():
-    parser = config_parser()
-    args = parser.parse_args()
-    
+def test(hn, hf, dataset, model, device='cuda', chunk_size=10, img_index=0, N_samples=192, H=400, W=400):
     """
-    llff dataset 불러오기 + setting
-    """
-    images, poses, bds, render_poses, i_test = load_llff_data(args.datadir, args.factor,
-                                                                      recenter=True, bd_factor=.75,
-                                                                      spherify=args.spherify)
-    
-    print("IMG", images.shape)
-    print("POSES", poses.shape)
-    print("bds", bds.shape)
-    print("render_poses", render_poses.shape)
-    print("i_test", i_test.shape)
-    
-    hwf = poses[0, :3, -1]  # height, width, focal length
-    poses = poses[:,:3,:4]  # camera extrinsic matrix
-    
-    if not isinstance(i_test, list):
-        i_test = [i_test]
-    
-    if args.llffhold > 0:
-            print('Auto LLFF holdout,', args.llffhold)
-            i_test = np.arange(images.shape[0])[::args.llffhold]
-    
-    i_val = i_test
-    i_train = np.array([i for i in np.arange(int(images.shape[0])) if
-                        (i not in i_test and i not in i_val)])
-    
-    print('Defining Bounds')
-    near = 0.
-    far = 1.
-    print('NEAR FAR', near, far)  # llff dataset에선 ndc(normalized device coordinates)를 사용하기 때문에 near -> 0  far -> 1로 설정
-    
-    """
-    instrinsics
-    """
-    H, W, focal = hwf
-    H, W = int(H), int(W)
-    hwf = [H, W, focal]
-    
-    K = np.array([
-        [focal, 0, 0.5*W],
-        [0, focal, 0.5*H],
-        [0, 0, 1]
-    ])
-    
-    """
-    Create log dir / make config file
-    """
-    basedir = args.basedir  # ./logs/
-    expname = "default"  # args.expname
-    os.makedirs(os.path.join(basedir, expname), exist_ok=True)
-    f = os.path.join(basedir, expname, 'args.txt')
-    with open(f, 'w') as file:
-        for arg in sorted(vars(args)):
-            attr = getattr(args, arg)
-            file.write('{} = {}\n'.format(arg, attr))
-    if args.config is not None:
-        f = os.path.join(basedir, expname, 'config.txt')
-        with open(f, 'w') as file:
-            file.write(open(args.config, 'r').read())
-    
-    """
-    Create NeRF model
-    """
-    render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer = create_nerf(args)
-    global_step = start
+    Args:
+        hn: near plane distance
+        hf: far plane distance
+        dataset: dataset to render
+        model: neural radiance model
+        chunk_size (int, optional): chunk size for memory efficiency. Defaults to 10.
+        img_index (int, optional): image index to render. Defaults to 0.
+        N_samples (int, optional): number of bins for density estimation. Defaults to 192.
+        H (int, optional): image height. Defaults to 400.
+        W (int, optional): image width. Defaults to 400.
 
-    bds_dict = {
-        'near': near,
-        'far': far,
-    }    
-    
-    render_kwargs_train.update(bds_dict)
-    render_kwargs_test.update(bds_dict)
+    Returns:
+        None: None
+    """
+    ray_origins = dataset[img_index * H * W: (img_index + 1) * H * W, :3]
+    ray_directions = dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
 
-    # Move testind data to GPU
-    render_poses = torch.Tensor(render_poses).to(device)
+    data = []   # list of regenerated pixel values
+    with torch.no_grad():
+        for i in range(int(np.ceil(H / chunk_size))):   # iterate over chunks
+            # Get chunk of rays
+            ray_origins_ = ray_origins[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)
+            ray_directions_ = ray_directions[i * W * chunk_size: (i + 1) * W * chunk_size].to(device)
+            regenerated_px_values = render_rays(model, ray_origins_, ray_directions_, device=device, hn=hn, hf=hf, N_samples=N_samples)
+            data.append(regenerated_px_values)
+        img = torch.cat(data).data.cpu().numpy().reshape(H, W, 3)
 
-    # rendering out from trained model
-    if args.render_only:
-        print("RENDER ONLY")
-        with torch.no_grad():
-            if args.render_test:
-                # render_test switches to test poses
-                images = images[i_test]
-            else:
-                # Default is smoother render_poses path
-                images = None
-    
+    plt.figure()
+    plt.imshow(img)
+    plt.savefig(f'novel_views/img_{img_index}.png', bbox_inches='tight')
+    plt.close()
+
+
+def train(nerf, optim, schedular, data_loader, device='cuda', near=0, far=1, epochs=int(1e5), N_samples=192, H=400, W=400):
+    training_loss = []
+    for _ in tqdm(range(epochs)):
+        for batch in data_loader:
+            rays_o = batch[:,:3].to(device)
+            rays_d = batch[:,3:6].to(device)
+            gt_pixels = batch[:,6:].to(device)
+
+            recon_pixels = render_rays(nerf, rays_o, rays_d, device, near, far, N_samples)
+            loss = ((gt_pixels - recon_pixels) ** 2).sum()
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            training_loss.append(loss.item())
+        schedular.step()
+
+        for img_index in range(200):
+            test(near, far, testing_dataset, device=device, img_index=img_index, N_samples=N_samples, H=H, W=W)
+
+    return training_loss
+
 
 if __name__=='__main__':
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    train()
+    train_dataset = torch.from_numpy(np.load('~~~~', allow_pickle=True))
+    testing_dataset = torch.from_numpy(np.load('~~~~', allow_pickle=True))
+
+    device = "cuda"
+
+    model = NeRF().to(device)
+    optim = torch.optim.Adam(model.parameters(), lr=5e-4)
+    schedular = torch.optim.lr_scheduler.MultiStepLR(optim, milestones=[2, 4, 8], gamma=0.5)
+    data_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True)
+    train(model, optim, schedular, data_loader, device=device, near=2, far=6, epochs=16, N_samples=192, H=400, W=400)
--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 
 공식 코드 및 구현된 코드를 참고하면서 모델의 알고리즘을 공부하는 목적의 레포입니다.
 
-Genarative 모델이나 NeRFs를 주로 다룰 예정이며 generative model 데이터셋으로는 celeba를 사용하고 있습니다. 
+Genarative 모델이나 NeRFs를 주로 다룰 예정입니다.
 
 ## plan
 
-* VAE (done)
-* VQ-VAE
 * NeRF (작성 중) -> 참고: https://github.com/yenchenlin/nerf-pytorch/tree/master
+    * 현재 coarse model까지만 코드 구현 완료, fine network까지 구현하기!
+* vae 코드 수정
+* ddpm 코드 구현
 * instant NGP
